f637790fe35233875cd27fcde349aaa2d0592598
Yacob U <43726344+axaa42@users.noreply.github.com>
2021-12-11 18:32:47 +0000

    Update ebay_api.py

--- Ebay_api/ebay_api.py
+++ Ebay_api/ebay_api.py
@@ -4,7 +4,7 @@ import requests
 r="xxx"#This is the token
 headers={"Authorization":r}#This is the auth
 #THE FOLLowing IS THE EBAY API.NOTE THAT YOU CAN PUT & TO GET THE ITEM YOU WANT
-d="""https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&RESPONSE-DATA-FORMAT=JSON&keywords=arsenal&SECURITY-APPNAME=xxx&paginationInput.entriesPerPage=25&paginationInput.pageNumber=1&GLOBAL-ID=EBAY-US"""#This is the url for ebay api.note that products per page and number of entries are put as queries.also you have to get appid which is in developer.also note you have to put what kind of api endpoint it is
+d="""xxxxxxxxxxx"""#This is the url for ebay api.note that products per page and number of entries are put as queries.also you have to get appid which is in developer.also note you have to put what kind of api endpoint it is
 #FOLLWOING ARE ANOTHER WAY OF PUTTING PARAMENTES IN THE URL
 g=request = {
     'paginationInput': {
